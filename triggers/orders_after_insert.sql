95390b49c67b2ef2199582fea1f85dde082ad4df
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2013-04-03 14:03:56 +0200

    Added deletion of drug entries on void in the flat table

--- triggers/orders_after_insert.sql
+++ triggers/orders_after_insert.sql
@@ -25,14 +25,17 @@ BEGIN
             IF @visit = 0 THEN
             
                 INSERT INTO flat_table2 (patient_id, visit_date, drug_order_id1, 
-                    drug_encounter_id1, drug_start_date1, drug_auto_expire_date1) 
+                    drug_encounter_id1, drug_start_date1, drug_auto_expire_date1, drug_order_id1_enc_id, 
+                    drug_encounter_id1_enc_id, drug_start_date1_enc_id, drug_auto_expire_date1_enc_id) 
                 VALUES (@patient_id, in_visit_date, new.order_id, new.encounter_id, 
-                    new.start_date, new.auto_expire_date);
+                    new.start_date, new.auto_expire_date, new.encounter_id, new.encounter_id, new.encounter_id, new.encounter_id);
             
             ELSE 
             
                 UPDATE flat_table2 SET drug_order_id1 = new.order_id, drug_encounter_id1 = new.encounter_id, 
-                    drug_start_date1 = new.start_date, drug_auto_expire_date1 = new.auto_expire_date 
+                    drug_start_date1 = new.start_date, drug_auto_expire_date1 = new.auto_expire_date,
+                    drug_order_id1_enc_id = new.encounter_id, drug_encounter_id1_enc_id = new.encounter_id, 
+                    drug_start_date1_enc_id = new.encounter_id, drug_auto_expire_date1_enc_id = new.encounter_id 
                 WHERE flat_table2.id = @visit;
                 
             END IF;                       
@@ -42,14 +45,17 @@ BEGIN
             IF @visit = 0 THEN
             
                 INSERT INTO flat_table2 (patient_id, visit_date, drug_order_id2, 
-                    drug_encounter_id2, drug_start_date2, drug_auto_expire_date2) 
+                    drug_encounter_id2, drug_start_date2, drug_auto_expire_date2, drug_order_id2_enc_id, 
+                    drug_encounter_id2_enc_id, drug_start_date2_enc_id, drug_auto_expire_date2_enc_id) 
                 VALUES (@patient_id, in_visit_date, new.order_id, new.encounter_id, 
-                    new.start_date, new.auto_expire_date);
+                    new.start_date, new.auto_expire_date, new.encounter_id, new.encounter_id, new.encounter_id, new.encounter_id);
             
             ELSE 
             
                 UPDATE flat_table2 SET drug_order_id2 = new.order_id, drug_encounter_id2 = new.encounter_id, 
-                    drug_start_date2 = new.start_date, drug_auto_expire_date2 = new.auto_expire_date 
+                    drug_start_date2 = new.start_date, drug_auto_expire_date2 = new.auto_expire_date ,
+                    drug_order_id2_enc_id = new.encounter_id, drug_encounter_id2_enc_id = new.encounter_id, 
+                    drug_start_date2_enc_id = new.encounter_id, drug_auto_expire_date2_enc_id = new.encounter_id
                 WHERE flat_table2.id = @visit;
                 
             END IF;                          
@@ -59,14 +65,17 @@ BEGIN
             IF @visit = 0 THEN
             
                 INSERT INTO flat_table2 (patient_id, visit_date, drug_order_id3, 
-                    drug_encounter_id3, drug_start_date3, drug_auto_expire_date3) 
+                    drug_encounter_id3, drug_start_date3, drug_auto_expire_date3, drug_order_id3_enc_id, 
+                    drug_encounter_id3_enc_id, drug_start_date3_enc_id, drug_auto_expire_date3_enc_id) 
                 VALUES (@patient_id, in_visit_date, new.order_id, new.encounter_id, 
-                    new.start_date, new.auto_expire_date);
+                    new.start_date, new.auto_expire_date, new.encounter_id, new.encounter_id, new.encounter_id, new.encounter_id);
             
             ELSE 
             
                 UPDATE flat_table2 SET drug_order_id3 = new.order_id, drug_encounter_id3 = new.encounter_id, 
-                    drug_start_date3 = new.start_date, drug_auto_expire_date3 = new.auto_expire_date 
+                    drug_start_date3 = new.start_date, drug_auto_expire_date3 = new.auto_expire_date,
+                    drug_order_id3_enc_id = new.encounter_id, drug_encounter_id3_enc_id = new.encounter_id, 
+                    drug_start_date3_enc_id = new.encounter_id, drug_auto_expire_date3_enc_id = new.encounter_id 
                 WHERE flat_table2.id = @visit;
                 
             END IF;                       
@@ -76,14 +85,17 @@ BEGIN
             IF @visit = 0 THEN
             
                 INSERT INTO flat_table2 (patient_id, visit_date, drug_order_id4, 
-                    drug_encounter_id4, drug_start_date4, drug_auto_expire_date4) 
+                    drug_encounter_id4, drug_start_date4, drug_auto_expire_date4, drug_order_id4_enc_id, 
+                    drug_encounter_id4_enc_id, drug_start_date4_enc_id, drug_auto_expire_date4_enc_id) 
                 VALUES (@patient_id, in_visit_date, new.order_id, new.encounter_id, 
-                    new.start_date, new.auto_expire_date);
+                    new.start_date, new.auto_expire_date, new.encounter_id, new.encounter_id, new.encounter_id, new.encounter_id);
             
             ELSE 
             
                 UPDATE flat_table2 SET drug_order_id4 = new.order_id, drug_encounter_id4 = new.encounter_id, 
-                    drug_start_date4 = new.start_date, drug_auto_expire_date4 = new.auto_expire_date 
+                    drug_start_date4 = new.start_date, drug_auto_expire_date4 = new.auto_expire_date,
+                    drug_order_id4_enc_id = new.encounter_id, drug_encounter_id4_enc_id = new.encounter_id, 
+                    drug_start_date4_enc_id = new.encounter_id, drug_auto_expire_date4_enc_id = new.encounter_id 
                 WHERE flat_table2.id = @visit;
                 
             END IF;                       
@@ -93,14 +105,17 @@ BEGIN
             IF @visit = 0 THEN
             
                 INSERT INTO flat_table2 (patient_id, visit_date, drug_order_id5, 
-                    drug_encounter_id5, drug_start_date5, drug_auto_expire_date5) 
+                    drug_encounter_id5, drug_start_date5, drug_auto_expire_date5, drug_order_id5_enc_id, 
+                    drug_encounter_id5_enc_id, drug_start_date5_enc_id, drug_auto_expire_date5_enc_id) 
                 VALUES (@patient_id, in_visit_date, new.order_id, new.encounter_id, 
-                    new.start_date, new.auto_expire_date);
+                    new.start_date, new.auto_expire_date, new.encounter_id, new.encounter_id, new.encounter_id, new.encounter_id);
             
             ELSE 
             
                 UPDATE flat_table2 SET drug_order_id5 = new.order_id, drug_encounter_id5 = new.encounter_id, 
-                    drug_start_date5 = new.start_date, drug_auto_expire_date5 = new.auto_expire_date 
+                    drug_start_date5 = new.start_date, drug_auto_expire_date5 = new.auto_expire_date,
+                    drug_order_id5_enc_id = new.encounter_id, drug_encounter_id5_enc_id = new.encounter_id, 
+                    drug_start_date5_enc_id = new.encounter_id, drug_auto_expire_date5_enc_id = new.encounter_id 
                 WHERE flat_table2.id = @visit;
                 
             END IF;  
