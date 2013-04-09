6f19220089677de5c248392d10998847553bd097
timmtonga <timmtonga@gmail.com>
2013-04-09 17:09:13 +0200

    added code to insert flat table 1 values in flat_cohort_table

--- triggers/flat_table1_after_update.sql
+++ triggers/flat_table1_after_update.sql
@@ -6,8 +6,23 @@ DELIMITER $$
 	
 	FOR EACH ROW BEGIN
 	
-		INSERT INTO flat_cohort_table (patient_id,gender) VALUES (new.patient_id, new.gender);
-	
+    SET @eligible = (SELECT earliest_start_date FROM flat_table1 WHERE patient_id = new.patient_id);
+    
+    IF @eligible IS NOT NULL THEN
+    	
+    	SET @record_exists = (SELECT gender FROM flat_cohort_table WHERE patient_id = new.patient_id);
+    		
+    		IF @record_exists IS NULL THEN
+						
+					INSERT INTO flat_cohort_table ( patient_id, gender, birthdate, earliest_start_date, reason_for_starting, ever_registered_at_art, date_art_last_taken, taken_art_in_last_two_months, extrapulmonary_tuberculosis, pulmonary_tuberculosis, pulmonary_tuberculosis_last_2_years, kaposis_sarcoma) VALUES (new.patient_id, new.gender, new.dob, @eligible, new.reason_for_eligibility, new.ever_registered_at_art_clinic, new.date_art_last_taken, new.taken_art_in_last_two_months, new.extrapulmonary_tuberculosis, new.pulmonary_tuberculosis, new.pulmonary_tuberculosis_last_2_years, new.kaposis_sarcoma);
+
+				ELSE
+				
+					UPDATE flat_cohort_table SET  earliest_start_date = @eligible, reason_for_starting = new.reason_for_eligibility, ever_registered_at_art = new.ever_registered_at_art_clinic, date_art_last_taken = new.date_art_last_taken, taken_art_in_last_two_months = new.taken_art_in_last_two_months, extrapulmonary_tuberculosis = new.extrapulmonary_tuberculosis, pulmonary_tuberculosis = new.pulmonary_tuberculosis, pulmonary_tuberculosis_last_2_years = new.pulmonary_tuberculosis_last_2_years, kaposis_sarcoma = new.kaposis_sarcoma WHERE patient_id = new.patient_id;
+
+				END IF;
+			
+		END IF;
 	END$$
 
 DELIMITER ;
