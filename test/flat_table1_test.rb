22d4cfc718bf83fc7efbfe9fda53a78357c72d2e
timmtonga <timmtonga@gmail.com>
2013-04-11 11:30:32 +0200

    fixed earliest start date bug and added code for database initialization

--- test/flat_table1_test.rb
+++ test/flat_table1_test.rb
@@ -15,106 +15,6 @@ class Con < Test::Unit::TestCase
 	
 	end
 	
-	def test_name_insert
-	
-	    date = Time.now.strftime("%Y-%m-%d")
-		
-		person = $con.query "INSERT INTO person (gender,birthdate,creator, uuid) VALUES ('F', #{date},1, #{rand(999999)})" ;
-		
-		name = $con.query "INSERT INTO person_name ( person_id, given_name, family_name, uuid, creator) VALUES ( 2,'James','Mbedza', (SELECT UUID()), 1)";
-					
-		names = $con.query "SELECT * FROM flat_table1 where given_name = 'James'"
-		
-		assert names.num_rows > 0 , "Failed to write names" rescue nil
-
-	end
-
-	def test_address_insert
-	
-		address = $con.query "INSERT INTO person_address (person_id, address2,creator) VALUES (2,'Mzimba',1)"
-		
-		checkers = $con.query "Select * from flat_table1 where home_district = 'Mzimba'"
-		
-		assert checkers.num_rows() > 0, "Test failed"
-	
-	end
-	
-	def test_identifiers_insert
-	
-		identifier = $con.query "INSERT INTO patient_identifier (patient_id, identifier, identifier_type,location_id ,creator) VALUES ( 2,'test-tb_number',7, 721,1)"
-		
-		checkers = $con.query "SELECT * FROM flat_table1 where tb_number is not null"
-		
-		assert checkers.num_rows > 0, "Test failed"
-	
-	end
-
-	def test_observations_insert
-	
-    date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-
-#		encounter_type_id = $con.query "Select encounter_type_id from encounter_type where name = 'hiv staging'"
-		
-		rs = $con.query "INSERT INTO encounter (encounter_type, patient_id,provider_id, encounter_datetime, creator, date_created, uuid) VALUES ( 52, 2, 1, '#{date}', 1, '#{date}', (SELECT UUID()))"
-
-		yes = 1065
-	 
-		fields = [ 8011, 2552,7754, 6394, 7752,7937, 6393]
-
-		fields.each do |field|
-			os = $con.query "INSERT INTO obs (person_id, concept_id, encounter_id,obs_datetime, value_coded, creator, date_created, uuid) VALUES (2, '#{field}', 1, '#{date}', '#{yes}', 1, '#{date}', (SELECT UUID()))"
-		end
-		checkers = $con.query "Select * from flat_table1 where has_transfer_letter is not null"
-		
-		assert checkers.num_rows() > 0, "Failed to write into table1"
-	end	
-	
-	
-	def test_encounter_update
-	
-		retired = $con.query "UPDATE encounter set voided = 1 where encounter_id = 1"
-	
-	end
-	
-	def test_person_address_void
-
-		retired = $con.query "UPDATE person_address set voided = 1 where person_id = 2"
-		
-		
-	end
-	
-	def test_person_address_update
-	
-		updated = $con.query "UPDATE person_address set city_village = 'AREA 15' where person_id = 2"
-		
-		check = $con.query "SELECT * from flat_table1 where current_address = 'AREA 15' and patient_id = 2"
-		
-		assert check.num_rows > 0, "failed to make changes"
-		
-	end
-	
-	def test_person_name_update
-	
-		retire = $con.query "UPDATE person_name set voided = 1 where person_id = 2"
-	
-	end
-	
-	def test_patient_identifier_update
-	
-		retire = $con.query "UPDATE patient_identifier SET voided = 1 WHERE identifier_type = 7 "
-	
-	end
-	
-	def test_patient_voided
-		
-		void = $con.query "UPDATE patient SET voided = 1 WHERE patient_id = 2"
-		
-		check = $con.query "SELECT * FROM flat_table1 where patient_id = 2"
-		
-		assert check.num_rows < 1, "Failed to remove patient record"
-		
-	end
-	
 	def test_complete_test
 	
 		user_id = 1
@@ -179,45 +79,6 @@ class Con < Test::Unit::TestCase
 	end
 	
 	
-	def test_stages_test
-	
-		stages = [7052, 7051, 7050, 7049,7048,7047,7046,7045,7044,7043,7042,7041]
-		
-		stages.each do |stage|
-		
-		
-			user_id = 1
-		
-			date = Time.now.strftime("%Y-%m-%d")
-		
-			person = $con.query "INSERT INTO person (gender,birthdate,creator, uuid) VALUES ('M', #{date},1, (SELECT UUID()))" ;
-	
-			is = $con.query "SELECT LAST_INSERT_ID()"
-				
-			person_id = is.fetch_row[0].to_i
-
-			name = $con.query "INSERT INTO person_name ( person_id, given_name, family_name, uuid, creator) VALUES ( #{person_id},'Test','Case', (SELECT UUID()), #{user_id})";
-
-			patient = $con.query "INSERT INTO patient (patient_id, creator, date_created, voided) VALUES (#{person_id}, #{user_id}, '#{date}', 0)"
-
-			check_patient = $con.query "Select * from patient where patient_id = #{person_id}"
-		
-			rs = $con.query "INSERT INTO encounter (encounter_type, patient_id,provider_id, encounter_datetime, creator, date_created, uuid) VALUES ( 52, #{person_id}, #{user_id}, '#{date}', #{user_id}, '#{date}', (SELECT UUID()))"
-
-			encounter = $con.query "SELECT LAST_INSERT_ID()"
-		
-			encounter_id = encounter.fetch_row[0].to_i
-			
-			os = $con.query "INSERT INTO obs (person_id, concept_id, encounter_id,obs_datetime, value_coded, creator, date_created, uuid) VALUES (#{person_id} ,7562, #{encounter_id}, '#{date}','#{stage}',#{user_id}, '#{date}', (SELECT UUID()))"
-		
-			check = $con.query "SELECT * from obs where encounter_id = #{encounter_id}"
-			
-			assert check.num_rows > 0, "Failed to write observations"
-						
-		end
-			
-	end
-	
 	def test_state_test 
 	
 		user_id = 1
@@ -265,6 +126,11 @@ class Con < Test::Unit::TestCase
       
       ps1 = $con.query "INSERT INTO patient_state (patient_program_id, state, start_date, " +
         "creator, date_created, voided, uuid) VALUES (#{patient_program_id}, #{program_workflow_state_id}, '2013-01-02', " +
+        " #{user_id} , '#{date}', 0, (SELECT UUID()))";
+
+
+   ps1 = $con.query "INSERT INTO patient_state (patient_program_id, state, start_date, " +
+        "creator, date_created, voided, uuid) VALUES (#{patient_program_id}, #{program_workflow_state_id}, '2014-04-01', " +
         " #{user_id} , '#{date}', 0, (SELECT UUID()))";
 
       
--- triggers/patient_after_void.sql
+++ triggers/patient_after_void.sql
@@ -11,7 +11,21 @@ DELIMITER $$
 				DELETE FROM flat_table1 where patient_id = new.patient_id;
 				DELETE FROM flat_table2 where patient_id = new.patient_id;
 				DELETE FROM flat_cohort_table where patient_id = new.patient_id;
-			
+
+			ELSE
+				
+					SET @check_existence = (SELECT patient_id FROM flat_table1 WHERE patient_id = new.patient_id);
+					
+					if @check_existence IS NULL THEN
+					
+							CALL `proc_insert_basics`(
+								new.patient_id,
+								new.date_created,
+								new.creator
+							); 					
+					
+					END IF;
+				
 			END IF;	
 				
 		END$$
