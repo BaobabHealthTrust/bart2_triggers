5937c1e57706ed3804133a686883e4781696dbb9
Deliwe <dbnkhoma@gmail.com>
2014-12-09 09:55:41 +0200

    updated insert_other_fields procedures

--- procedures/sub-procedures/inserts/proc_insert_other_field.sql
+++ procedures/sub-procedures/inserts/proc_insert_other_field.sql
@@ -68,6 +68,14 @@ BEGIN
     SET @condoms = (SELECT concept_name.concept_id FROM concept_name 
         LEFT OUTER JOIN concept ON concept.concept_id = concept_name.concept_id 
         WHERE name = "Condoms" AND voided = 0 AND retired = 0 LIMIT 1);
+
+    SET @cpt_given = (SELECT concept_name.concept_id FROM concept_name 
+        LEFT OUTER JOIN concept ON concept.concept_id = concept_name.concept_id 
+        WHERE name = "CPT started" AND voided = 0 AND retired = 0 LIMIT 1);        
+
+   SET @ipt_given = (SELECT concept_name.concept_id FROM concept_name 
+        LEFT OUTER JOIN concept ON concept.concept_id = concept_name.concept_id 
+        WHERE name = "Isoniazid" AND voided = 0 AND retired = 0 LIMIT 1);        
     
     SET @amount_of_drug_brought_to_clinic = (SELECT concept_name.concept_id FROM concept_name 
         LEFT OUTER JOIN concept ON concept.concept_id = concept_name.concept_id 
@@ -790,10 +798,10 @@ BEGIN
       
       ##########################################################################################################################################
       
-          WHEN @condoms THEN
+          WHEN @cpt_given THEN
             IF in_field_voided = 0 THEN
               IF in_visit_id = 0 THEN
-              
+                
                   INSERT INTO flat_table2 (patient_id, visit_date, condoms_given, condoms_given_enc_id) VALUES (in_patient_id, in_visit_date, in_field_value_text, encounter_id);
               
               ELSE 
@@ -917,7 +925,7 @@ BEGIN
               
               ELSE 
                 UPDATE flat_table2 SET systolic_blood_pressure = in_field_value_numeric, systolic_blood_pressure_enc_id = encounter_id WHERE flat_table2.id = in_visit_id;
-                  
+     
               END IF;
             ELSE
                UPDATE flat_table2 SET systolic_blood_pressure = NULL, systolic_blood_pressure_enc_id = NULL WHERE flat_table2.id = in_visit_id; 
