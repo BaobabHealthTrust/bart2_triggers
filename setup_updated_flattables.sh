d441d23da3ced8a70cd652f19830d32d6b94d743
Deliwe Nkhoma <dbnkhoma@gmail.com>
2016-11-21 15:44:29 +0200

    flat_tables revised modifications

--- setup_updated_flattables.sh
+++ setup_updated_flattables.sh
@@ -21,9 +21,10 @@ do
 	mysql --user=$USER --password=$PASS $DB < $f
 done
 
-FILES=triggers/updated_flat_tables/*.sql
-for f in $FILES
-do
-	echo "Installing $f..."
-	mysql --user=$USER --password=$PASS $DB < $f
-done
+
+#FILES=triggers/updated_flat_tables/*.sql
+#for f in $FILES
+#do
+#	echo "Installing $f..."
+#	mysql --user=$USER --password=$PASS $DB < $f
+#done
